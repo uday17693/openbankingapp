9b0e6648b0333c09c1ff9b620b73e7e0e1e536e4
uday17693 <uday17693@gmail.com>
2019-05-11 12:55:19 +0200

    Added first compoent

---
+++ src/app.js
@@ -0,0 +1,6 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import AppTitle from "./components/Title";
+
+
+ReactDOM.render(<AppTitle></AppTitle>, document.getElementById("root"));
---
+++ src/components/Title.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+class AppTitle extends React.Component {
+  constructor(props) {
+    super(props);    
+  }
+  
+  render() {
+    return (
+      <p>Hello this is my first react component!</p>
+    );
+  }
+}
+export default AppTitle;
